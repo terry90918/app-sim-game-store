4bb48c9fbc3fa849f9e084d2e22a7d84e53b2103
Terry.TY <zxtw17985321@gmail.com>
2019-02-22 15:53:36 +0800

    建立訂單與表單驗證技巧

--- src/views/admin/CustomerOrders.vue
+++ src/views/admin/CustomerOrders.vue
@@ -124,8 +124,9 @@
         </div>
       </div>
     </div>
+    <hr />
     <div class="my-5 row justify-content-center">
-      <div class="my-5 row justify-content-center">
+      <div class="col-6">
         <table class="table">
           <thead>
             <th></th>
@@ -186,6 +187,87 @@
         </div>
       </div>
     </div>
+    <hr />
+    <div class="my-5 row justify-content-center">
+      <form class="col-md-6" @submit.prevent="createOrder">
+        <div class="form-group">
+          <label for="useremail">Email</label>
+          <input
+            id="useremail"
+            class="form-control"
+            type="email"
+            name="email"
+            placeholder="請輸入 Email"
+            :class="{ 'is-invalid': errors.has('email') }"
+            v-model="form.user.email"
+            v-validate="'required|email'"
+          />
+          <span class="text-danger" v-if="errors.has('email')">
+            {{ errors.first("email") }}
+          </span>
+        </div>
+
+        <div class="form-group">
+          <label for="username">收件人姓名</label>
+          <input
+            id="username"
+            class="form-control"
+            type="text"
+            name="name"
+            placeholder="輸入姓名"
+            :class="{ 'is-invalid': errors.has('name') }"
+            v-model="form.user.name"
+            v-validate="'required'"
+          />
+          <span class="text-danger" v-if="errors.has('name')">
+            姓名必須輸入
+          </span>
+        </div>
+
+        <div class="form-group">
+          <label for="usertel">收件人電話</label>
+          <input
+            type="tel"
+            class="form-control"
+            id="usertel"
+            v-model="form.user.tel"
+            placeholder="請輸入電話"
+          />
+        </div>
+
+        <div class="form-group">
+          <label for="useraddress">收件人地址</label>
+          <input
+            type="address"
+            class="form-control"
+            name="address"
+            :class="{ 'is-invalid': errors.has('address') }"
+            id="useraddress"
+            v-model="form.user.address"
+            v-validate="'required'"
+            placeholder="請輸入地址"
+          />
+          <span class="text-danger" v-if="errors.has('address')">
+            地址欄位不得留空
+          </span>
+        </div>
+
+        <div class="form-group">
+          <label for="useraddress">留言</label>
+          <textarea
+            name=""
+            id=""
+            class="form-control"
+            cols="30"
+            rows="10"
+            v-model="form.message"
+          ></textarea>
+        </div>
+        <div class="text-right">
+          <button class="btn btn-danger">送出訂單</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -198,6 +280,15 @@ export default {
     return {
       cart: {},
       coupon_code: "",
+      form: {
+        message: "",
+        user: {
+          address: "",
+          email: "",
+          name: "",
+          tel: ""
+        }
+      },
       isLoading: false,
       product: {},
       products: [],
@@ -248,6 +339,32 @@ export default {
           $("#modal-edit").modal("hide");
         } else {
           vm.$bus.$emit("messsage:push", response.data.message, "danger");
+        }
+      });
+    },
+    // 結帳頁面
+    // 建立訂單後會把所選的購物車資訊刪除, message 欄位為必填, user 物件為必要
+    createOrder() {
+      const vm = this;
+      const api = `${process.env.VUE_APP_API}/api/${
+        process.env.VUE_APP_API_PATH
+      }/order`;
+      const order = vm.form;
+
+      vm.isLoading = true;
+      vm.$validator.validate().then(result => {
+        if (result) {
+          vm.axios.post(api, { data: order }).then(response => {
+            vm.isLoading = false;
+            if (response.data.success) {
+              vm.$bus.$emit("messsage:push", "訂單已建立", "success");
+              vm.getCart();
+            } else {
+              vm.$bus.$emit("messsage:push", response.data.message, "danger");
+            }
+          });
+        } else {
+          vm.$bus.$emit("messsage:push", "欄位不完整", "danger");
         }
       });
     },
