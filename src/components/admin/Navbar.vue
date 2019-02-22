6552aad96f705b46dbedb7567d33fdb47b1b9ecd
Terry.TY <zxtw17985321@gmail.com>
2019-02-22 14:59:38 +0800

    標題調整

--- src/components/admin/Navbar.vue
+++ src/components/admin/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
     <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="/#/admin">
-      Company name
+      管理後台
     </a>
     <input
       class="form-control form-control-dark w-100"
